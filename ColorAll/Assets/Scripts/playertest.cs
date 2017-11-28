d445dfe3df02b0689262742769d2719489991a0f
victorcezario97 <victorscezario@hotmail.com>
2017-11-28 15:22:25 -0200

    Adding player prototype

---
+++ ColorAll/Assets/Scripts/playertest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playertest : MonoBehaviour {
+
+    public float movSpeed;
+    public float jumpHeight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if(GetComponent<Rigidbody2D>().velocity.y == 0)
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-movSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+}
