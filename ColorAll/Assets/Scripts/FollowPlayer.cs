36860afdcd37e96d0eed87e30ea7e8ac3d008ae4
Leonardo Cesar <guy.fawkes.05111605.e@gmail.com>
2017-11-30 14:20:26 -0200

    Improved movement system. Player can no longer get stuck on walls (mostly)

---
+++ ColorAll/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start() 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		Vector3 playerPosition = FindObjectOfType<Player>().transform.position;
+		transform.position = new Vector3(playerPosition.x, playerPosition.y, transform.position.z);
+	}
+}
---
+++ ColorAll/Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+    [HideInInspector] public bool facingRight = true;
+    [HideInInspector] public bool jump = true;
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
+
+    private bool grounded = false;
+    private Rigidbody2D rigidbody2D;
+
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update() 
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Scene"));
+
+        Debug.Log(grounded);
+        if (Input.GetKeyDown(KeyCode.W) && grounded)
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        
+        if (h * rigidbody2D.velocity.x < maxSpeed)
+        {
+            rigidbody2D.AddForce(Vector2.right * h * moveForce);
+        }
+
+        if (Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+        {
+            rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+        }
+
+        if ((h > 0 && !facingRight) || (h < 0 && facingRight))
+        {
+            Flip();
+        }
+
+        if (jump)
+        {
+            rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
