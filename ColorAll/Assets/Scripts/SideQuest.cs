f86ef20d0446168831fbdfcad17e0bfdb1625bb6
victorcezario97 <victorscezario97@gmail.com>
2017-12-09 01:07:49 -0200

    Added warning speech bubble

--- ColorAll/Assets/Scripts/SideQuest.cs
+++ ColorAll/Assets/Scripts/SideQuest.cs
@@ -7,9 +7,9 @@ public class SideQuest : MonoBehaviour {
     public GameObject paint;
     public GameObject speechBubble;
     public GameObject arrow;
-   // public GameObject droplet;
-   // public GameObject speechBubbleWarning;
-    //public GameObject x;
+    public GameObject droplet;
+    public GameObject speechBubbleWarning;
+    public GameObject x;
 
     // Use this for initialization
     void Start () {
@@ -36,9 +36,9 @@ public class SideQuest : MonoBehaviour {
         arrow.GetComponent<SpriteRenderer>().enabled = true;
         paint.GetComponent<SpriteRenderer>().enabled = true;
         speechBubble.GetComponent<SpriteRenderer>().enabled = true;
-        //speechBubbleWarning.GetComponent<SpriteRenderer>().enabled = true;
-        //droplet.GetComponent<SpriteRenderer>().enabled = true;
-        //x.GetComponent<SpriteRenderer>().enabled = true;
+        speechBubbleWarning.GetComponent<SpriteRenderer>().enabled = true;
+        droplet.GetComponent<SpriteRenderer>().enabled = true;
+        x.GetComponent<SpriteRenderer>().enabled = true;
     }
 
     private void hideSpeechBubble()
@@ -46,8 +46,8 @@ public class SideQuest : MonoBehaviour {
         arrow.GetComponent<SpriteRenderer>().enabled = false;
         paint.GetComponent<SpriteRenderer>().enabled = false;
         speechBubble.GetComponent<SpriteRenderer>().enabled = false;
-        //speechBubbleWarning.GetComponent<SpriteRenderer>().enabled = false;
-        //droplet.GetComponent<SpriteRenderer>().enabled = false;
-        //x.GetComponent<SpriteRenderer>().enabled = false;
+        speechBubbleWarning.GetComponent<SpriteRenderer>().enabled = false;
+        droplet.GetComponent<SpriteRenderer>().enabled = false;
+        x.GetComponent<SpriteRenderer>().enabled = false;
     }
 }
