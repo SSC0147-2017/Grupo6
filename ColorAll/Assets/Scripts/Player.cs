d07297faf06ac9b064b8aab3573f8b0d68e8620f
victorcezario97 <victorscezario97@gmail.com>
2017-12-13 01:31:32 -0200

    Scene now restarts if health reaches 0

--- ColorAll/Assets/Scripts/Player.cs
+++ ColorAll/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour 
 {
@@ -156,5 +157,6 @@ public class Player : MonoBehaviour
     public void InflictDamage(float damage)
     {
         health -= damage;
+        if (health <= 0) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
